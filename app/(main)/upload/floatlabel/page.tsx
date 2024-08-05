cf933071d3f926e1fcb9e69764243251de51d902
Tong Thanh <tongthanhkt@gmail.com>
2024-08-05 12:59:06 +0700

    fix: workflows be

--- app/(main)/upload/floatlabel/page.tsx
+++ app/(main)/upload/floatlabel/page.tsx
@@ -10,7 +10,7 @@ import { InputText } from 'primereact/inputtext';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { MultiSelect } from 'primereact/multiselect';
 import { useEffect, useState } from 'react';
-import { CountryService } from '../../../../../demo/service/CountryService';
+import { CountryService } from '../../../../demo/service/CountryService';
 
 const FloatLabelDemo = () => {
     const [countries, setCountries] = useState<Demo.Country[]>([]);
